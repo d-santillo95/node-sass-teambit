09b30722bd36d4378bac91df44e1d3f36516d2fe
Daniele Santillo <daniele.santillo95@gmail.com>
2020-06-10 10:31:41 +0100

    aggiunti piccoli dettagli

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap/js/src/collapse.js');
 // require('bootstrap/js/src/dropdown.js');
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            $('header').addClass('scrolled');
+            $('.navbar').addClass('scrolled');
+            $('.btn-header').addClass('scrolled')
+        } else {
+            $('header').removeClass('scrolled');
+            $('.navbar').removeClass('scrolled');
+            $('.btn-header').removeClass('scrolled')
+        }
+    });
+})
